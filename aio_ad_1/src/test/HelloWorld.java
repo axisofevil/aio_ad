0d286149634610e8571518444522f23fdb15a88b
axisofevil <turss.dev@googlemail.com>
2012-07-01 08:39:32 +0300

    Update master

--- aio_ad_1/src/test/HelloWorld.java
+++ aio_ad_1/src/test/HelloWorld.java
@@ -7,7 +7,9 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Welt");
+		//Verarscht
+		System.out.println("Hallo Welt 345");
+		System.out.println("Welt Hallo");
 	}
 
 }
